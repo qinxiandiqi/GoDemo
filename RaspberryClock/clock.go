7d8cd1a897d0d8bb3f29607020c94fa2e078e8da
Jianan <qinxiandiqi@foxmail.com>
2015-12-30 00:07:57 +0800

    add GPIO library.

--- RaspberryClock/clock.go
+++ RaspberryClock/clock.go
@@ -1,4 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"github.com/stianeikeland/go-rpio"
+	"os"
+	"time"
+)
+
+var (
+
+)
+
 func main() {
+	if err := rpio.Open(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	defer rpio.Close()
+
+	time.Now()
 }
